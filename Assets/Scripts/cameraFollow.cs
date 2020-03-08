c77549e12f1e669c59e8a95bf55a9c0fd3e25ea8
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-03-08 12:18:22 -0600

    finished camera

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class cameraFollow : MonoBehaviour
 {
 
-    public float speed = 40f;
+    // selects targe to be fixed on
+    public Transform target;
+    public float smoothing = 0.12f;
 
+    // because it is fixed to player, we want to move in from by 10 layers be default
+    public Vector3 offset = new Vector3(0f,0f,-10f);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        Vector3 desiredP = target.position + offset;
+
+        // interpolate movement
+        Vector3 smoothP = Vector3.Lerp(transform.position, desiredP, smoothing);
+        transform.position = smoothP;
+
+        // will move in direction of target
+        // comment out if you don't like the jitters
+        transform.LookAt(target);
     }
+
 }
