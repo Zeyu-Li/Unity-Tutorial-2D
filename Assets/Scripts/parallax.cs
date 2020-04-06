64f449e20fd2328aec84f8574103ded5800d759d
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-04-06 15:29:21 -0600

    finished parallax

---
+++ Assets/Scripts/parallax.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour
+{
+    private float length;
+    private float startpos;
+
+    public GameObject cam;
+    public float parallex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // if you want to do it by the y, just change x to y
+        startpos = transform.position.x;
+
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dis = cam.transform.position.x * parallex;
+        float tmp = cam.transform.position.x * (1-parallex);
+
+        transform.position = new Vector3(startpos + dis, transform.position.y, transform.position.z);
+
+        if (tmp > startpos+length) {
+            startpos += length;
+        } else if (tmp < startpos - length) {
+            startpos -= length;
+        }
+    }
+}
