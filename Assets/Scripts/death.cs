2f1217397dd8d367b7e16250d457dafed894c0f4
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-04-07 11:28:20 -0600

    finished death zone

---
+++ Assets/Scripts/death.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class death : MonoBehaviour
+{
+    // collides with object tagged death
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "death") {
+            Die();
+        }
+    }
+
+    // kills player and resets
+    void Die() {
+        Destroy(transform.parent.gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
